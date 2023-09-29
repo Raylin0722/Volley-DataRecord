5271b2c4f916d08a84d454800b91e315dbc1a16b
Raylin0722 <107912521+Raylin0722@users.noreply.github.com>
2023-09-29 08:22:53 +0800

    處理資料庫

--- Assets/C# script/dealDB.cs
+++ Assets/C# script/dealDB.cs
@@ -39,7 +39,7 @@ public class dealDB : MonoBehaviour
         //insertData(testing);
         //insertData(testing);
 
-        //displayData();
+        displayData();
     }
 
     // Update is called once per frame
