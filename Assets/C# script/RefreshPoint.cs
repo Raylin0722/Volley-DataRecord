fa15432bf957ad7024932246deadf6598a077c44
Z4za01 <ugughr63@gmail.com>
2023-09-29 16:38:34 +0800

    錯誤的加分按鈕

--- Assets/C# script/RefreshPoint.cs
+++ Assets/C# script/RefreshPoint.cs
@@ -10,18 +10,24 @@ public class RefreshPoint : MonoBehaviour
     [SerializeField] Text Self_Point_Text;
     [SerializeField] int They_Point;
     [SerializeField] Text They_Point_Text;
+    [SerializeField] int Self_Score;
+    [SerializeField] Text Self_Score_Text;
+    [SerializeField] int They_Score;
+    [SerializeField] Text They_Score_Text;
     
     // Start is called before the first frame update
     void Start()
     {
+        Self_Score = 0;
         Self_Point = 0;   
         They_Point = 0;
+        They_Score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+       
     }
 
     public void add_point(){
@@ -34,6 +40,7 @@ public class RefreshPoint : MonoBehaviour
             else{
                 Self_Point_Text.text = Self_Point.ToString();
             }
+            add_score();
         }
         else{
             They_Point++;
@@ -43,6 +50,18 @@ public class RefreshPoint : MonoBehaviour
             else{
                 They_Point_Text.text = They_Point.ToString();
             }
+            add_score();
+        }
+    }
+
+    public void add_score(){
+        if(Self_Point >= 25 && ((Self_Point - They_Point) >= 2)){
+            Self_Score++;
+            Self_Score_Text.text = Self_Score.ToString();
+        }
+        else if(They_Point >= 25 && ((They_Point - Self_Point) >= 2)){
+            They_Score++;
+            They_Score_Text.text = They_Score.ToString();
         }
     }
    
