bf34134415cb83a07d6e362fbcca60a876af94f7
Raylin0722 <107912521+Raylin0722@users.noreply.github.com>
2023-12-04 19:58:35 +0800

    修改伺服器位置 安裝伺服器所需插件 新增"依照使用者輸入建立表"函式

--- Assets/Server/Server.py
+++ Assets/Server/Server.py
@@ -5,6 +5,7 @@ import hashlib
 from datetime import datetime, timedelta
 import json
 import sys
+import re
 
 app = Flask(__name__)
 
@@ -40,11 +41,11 @@ def login():
     cur.close()
     cnx.close()
 
-
 @app.route("/register")
 def register():
     account = request.form.get("account")
     password = request.form.get("password")
+        
 
     cnx = mysql.connector.connect(**config)
     cur = cnx.cursor(buffered=True)
@@ -54,17 +55,50 @@ def register():
 
     returnResult = {"success" : False}
 
-    if len(result) == 0:
+    if len(result) == 0 and re.match("^[a-zA-Z0-9]+$", account):
         hashResult = hashlib.sha256(password.encode("utf-8")).hexdigest()
         cur.execute("insert into users(account, hash) value(%s, %s);", (account, hashResult))
         cnx.commit()
-        returnResult['success'] = True
-
-    cur.close()
-    cnx.close()
+        try:
+            cur.execute(f"create table {account} (ID INT AUTO_INCREMENT PRIMARY KEY, gameName VARCHAR(50));")
+        except:
+            returnResult['success'] = False
+        else:
+            returnResult['success'] = True
+        finally:
+            cur.close()
+            cnx.close()
 
     return returnResult
 
 @app.route("/initDB")
 def initDB():
-    ()
+    gameName = request.form.get("gameName")
+    account = request.form.get("account")
+    
+    cnx = mysql.connector.connect(**config)
+    cur = cnx.cursor(buffered=True)
+
+    resultReturn = {"success" : False}
+
+    try:
+        cur.execute(f"show tables like {account};")
+        result = cur.fetchall()
+        if result and re.match("^[a-zA-Z0-9]+$", account) and re.match("^[a-zA-Z0-9]+$", gameName): 
+            cur.execute("select * from {account} where gameName=%s", (gameName, ))
+            checkGameExist = cur.fetchall()
+            if len(checkGameExist) == 0:
+                cur.execute("insert into `{account}`(`gameName`) value(%s);", (account+'_'+gameName, ))
+                cnx.commit()
+                resultReturn['success'] = True
+    except:
+        resultReturn['success'] = False
+    finally:
+        cur.close()
+        cnx.close()
+        
+    return resultReturn
+    
+
+    
+    
