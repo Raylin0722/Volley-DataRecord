c60186e1bdd680f2409759cced8b176b6fd16485
Raylin0722 <107912521+Raylin0722@users.noreply.github.com>
2023-12-05 03:40:20 +0800

    大幅修改資料庫

--- Assets/C# script/dealDB.cs
+++ Assets/C# script/dealDB.cs
@@ -5,6 +5,8 @@ using TMPro;
 using System.Data;
 using Mono.Data.Sqlite;
 using System;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
 
 
 public class dealDB : MonoBehaviour
@@ -18,22 +20,21 @@ public class dealDB : MonoBehaviour
     public const int SERVE = 1;
     public const int ATTACK = 2;
     public const int BLOCK = 3;
+    public const int SCORE = 4;
     
     public string gameName;
     public string account;
     public string hashPasswd;
     public struct Data{
         public string formation;
-        public int index;
         public int round;
         public string role;
         public string attackblock, catchblock;
         public int score, situation;
 
-        public Data(string formation, int index, int round, string role, string attackblock,
+        public Data(string formation,  int round, string role, string attackblock,
                     string catchblock, int situation, int score){
             this.formation = formation;
-            this.index = index;
             this.round = round;
             this.role = role;
             this.attackblock = attackblock;
@@ -46,18 +47,16 @@ public class dealDB : MonoBehaviour
     public class Return{
         public bool success;
     }
-    public Data[] saveData; // 儲存資料用
-    public int[] saveIndex; // 儲存資料用
-    public Data[] showData; // 顯示資料用
+    public List<Data> saveData; // 儲存資料用 
+    public List<Data> showData; // 顯示資料用
 
     // Start is called before the first frame update
     void Awake(){
-        saveData = new Data[300]; // 儲存資料用
-        saveIndex = new int[1]; // 儲存資料用
+        saveData = new List<Data>(); // 儲存資料用
     }
 
     public void CallinitDB(string gameName){
-        
+        StartCoroutine(initDB());
     }
 
     IEnumerator initDB(){
@@ -81,22 +80,59 @@ public class dealDB : MonoBehaviour
 
     }
 
-    public void displayData(){
+    public void CallDisplayData(){
+        StartCoroutine(displayData());
+    }
+
+    IEnumerator displayData(){
         WWWForm form = new WWWForm();
         form.AddField("gameName", gameName);
         form.AddField("account", account);
 
+        
         UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:5000/displayData", form);
         yield return www.SendWebRequest();
 
         if(www.result == UnityWebRequest.Result.Success){
             string response = www.downloadHandler.text;
-            showData = JsonUtility.FromJson<Data[]>(response);
+            showData = JsonConvert.DeserializeObject<List<Data>>(response);
         }
     }
 
-    public void insertData(){
-        
+    public void CallInsertData(){
+        StartCoroutine(insertData());
+    }
+
+    IEnumerator insertData()
+    {
+        string data = JsonConvert.SerializeObject(saveData);
+        string serverUrl = "http://127.0.0.1:5000/insertData";
+
+        // 创建 UnityWebRequest 对象
+        using (UnityWebRequest www = UnityWebRequest.PostWwwForm(serverUrl, ""))
+        {
+            // 设置请求头部为 application/json
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            // 将 JSON 数据放入请求的数据体
+            byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(data);
+            www.uploadHandler = new UploadHandlerRaw(jsonBytes);
+
+            // 发送网络请求
+            yield return www.SendWebRequest();
+
+            // 处理响应
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("POST request successful");
+            }
+            else
+            {
+                Debug.LogError($"Error: {www.error}");
+            }
+        }
+
+        saveData.Clear();
     }
 
 }
